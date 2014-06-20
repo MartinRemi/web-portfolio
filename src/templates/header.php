536537653806dfe39ce890dd4b0aad1e0e4a95ba
RemiMartin <mrmi11@yahoo.fr>
2014-06-20 23:37:09 +0200

    Changed header

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,5 +1,35 @@
 <header>
-	<?php
-		echo '<img src="' . $LOGO . '" alt="' . $LOGO_ALT . '"/>';
-	?>
+	<nav class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="container">
+
+			<!-- LOGO -->
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">
+					<?php
+						echo '<img src="' . $LOGO . '" alt="' . $LOGO_ALT . '" width="50" />';
+					?>
+				</a>
+			</div>
+
+			<!-- Drop-down language -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							Dropdown
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			
+		</div>
+	</nav>
 </header>
