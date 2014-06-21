73a668fcf9561881f76e80dc07fa697401d4f031
RemiMartin <mrmi11@yahoo.fr>
2014-06-21 13:55:56 +0200

    Fixed responsive stuff

--- src/templates/content.php
+++ src/templates/content.php
@@ -1,7 +1,7 @@
 <div class="col-md-8">
 	<div class="jumbotron">
-		<h1>Hello, world!</h1>
-		<p>...</p>
+		<h1>The lab</h1>
+		<img src="images/flask.png" width="100%" />
 		<p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
 	</div>
 </div>
--- src/templates/header.php
+++ src/templates/header.php
@@ -1,6 +1,6 @@
 <header>
-	<nav class="navbar navbar-default navbar-static-top" role="navigation">
-		<div class="container">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
 
 			<!-- LOGO -->
 			<div class="navbar-header">
@@ -10,20 +10,43 @@
 					?>
 				</a>
 			</div>
+			
+			<!-- Used when collapsing -->
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 
 			<!-- Drop-down language -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Index</a></li>
+					<li>
+						<a href="#">
+							<span class="glyphicon glyphicon-home"></span> Index
+						</a>
+					</li>
 					<li>
 						<a href="#">
 							<span class="glyphicon glyphicon-user"></span> Profile
 						</a>
 					</li>
-					<li><a href="#">Lab</a></li>
-					<li><a href="#">Projects</a></li>
+					<li>
+						<a href="#">
+							<span class="glyphicon">
+								<img src="images/flaskSprite.png" width="16"/>
+							</span> Lab
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="glyphicon glyphicon-briefcase"></span> Projects
+						</a>
+					</li>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<span class="glyphicon glyphicon-globe"></span>
 							<?php
 								echo $LANGUAGE;
 							?>
