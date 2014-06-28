2facd6d7530026167b2ef3a0ca7af60442ad85bf
RemiMartin <mrmi11@yahoo.fr>
2014-06-28 11:54:15 +0200

    Fixed problem with mail

--- src/tools/sendMail.php
+++ src/tools/sendMail.php
@@ -3,10 +3,12 @@
 	$email = $_POST['email'];
 	$subject = $_POST['subject'];
 	$content = $_POST['content'];
+	header('Content-type: application/json'); 
+	
 	$to = 'martinremi60@gmail.com';
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 	$headers .= "From: $email";
 	$ok = mail($to, $subject, $content, $headers);
-	header('Content-type: application/json'); echo $ok ? json_encode(array("status" => true)) : json_encode("status" => false));
+	echo $ok ? json_encode(array("status" => true)) : json_encode(array("status" => false));
 ?>
