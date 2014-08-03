faff193239ef6468817201d26cc1155d05c5fd65
RemiMartin <mrmi11@yahoo.fr>
2014-08-03 16:56:17 +0200

    Added news model

---
+++ src/application/models/newsModel.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ *	Model 'User', used to manipulate user related data from the database
+ */
+class NewsModel extends CI_Model
+{
+	// ----- Static Member(s)
+	protected $table = 'news';
+
+	// ----- Method(s)
+	/**
+	 *	Insert a new row in the database
+	 *	@return true if a new row has been added
+	 *			else false
+	 */
+	public function insert($author, $title, $content, $date)
+	{
+		//	Set query parameters
+		$this->db->set('author', $author);
+		$this->db->set('title', $title);
+		$this->db->set('content', sha1($content));
+		$this->db->set('date', date('Y-m-d H:i:s')); // Now
+
+		//	Insert
+		return $this->db->insert($this->table);
+	}
+
+	/**
+	 *	Return the user with the id equals to $id
+	 */
+	public function retrieve($id)
+	{
+		return $this->db
+					->select('*')
+					->from($this->table)
+					->where('id', (int)$id)
+					->get()
+					->result();
+	}
+
+	public function retrieveAll() {
+		return $this->db
+					->select('*')
+					->from($this->table)
+					->get()
+					->result();
+	}
+}
